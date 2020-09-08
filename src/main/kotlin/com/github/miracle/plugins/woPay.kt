f674b8f8dccdadcf53c0b28f97190cbf1d201e81
jinserrr <cmdr.jv@gmail.com>
2020-09-08 23:05:40 +0800

    QQ group

--- src/main/kotlin/com/github/miracle/plugins/woPay.kt
+++ src/main/kotlin/com/github/miracle/plugins/woPay.kt
@@ -10,6 +10,7 @@ import net.mamoe.mirai.event.EventPriority
 import net.mamoe.mirai.event.events.BotJoinGroupEvent
 import net.mamoe.mirai.event.subscribeAlways
 import net.mamoe.mirai.event.subscribeGroupMessages
+import net.mamoe.mirai.getGroupOrNull
 import net.mamoe.mirai.message.data.At
 import net.mamoe.mirai.message.data.PlainText
 import net.mamoe.mirai.message.data.content
@@ -31,6 +32,7 @@ fun Bot.woPay() {
             if (date.isBefore(LocalDate.now())) {
                 launch {
                     delay(TimeUnit.SECONDS.toMillis(25))
+                    group.sendMessage("bot已到期, 如有意愿续费请加群:\n117340135")
                     group.quit()
                 }
             }
