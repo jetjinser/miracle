a816c58bf7c2fc5c943477a36355a4eb5aa328f2
jinserrr <cmdr.jv@gmail.com>
2020-11-20 19:04:07 +0800

    fix: reaction 修复了没有自动同意入群的bug

--- src/main/kotlin/com/github/miracle/plugins/reaction.kt
+++ src/main/kotlin/com/github/miracle/plugins/reaction.kt
@@ -6,9 +6,9 @@ import com.github.miracle.utils.data.WoPayData
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import net.mamoe.mirai.Bot
-import net.mamoe.mirai.contact.nameCardOrNick
 import net.mamoe.mirai.event.Listener
-import net.mamoe.mirai.event.events.*
+import net.mamoe.mirai.event.events.BotInvitedJoinGroupRequestEvent
+import net.mamoe.mirai.event.events.MemberJoinEvent
 import net.mamoe.mirai.event.subscribeAlways
 import net.mamoe.mirai.getFriendOrNull
 import net.mamoe.mirai.getGroupOrNull
@@ -17,20 +17,23 @@ import java.time.format.DateTimeFormatter
 import java.util.concurrent.TimeUnit
 
 fun Bot.reaction() {
-//    subscribeAlways<GroupEntranceAnnouncementChangeEvent> {
-//        group.sendMessage("🔈 群公告已改变, 请及时查看\nchange by ${operatorOrBot.nameCardOrNick}")
-//    }
+    subscribeAlways<MemberJoinEvent> {
+        group.sendMessage("欢迎")
+        // TODO 自定义
+    }
 
-    subscribeAlways<BotJoinGroupEvent> {
-        group.sendMessage("大家好")
-        val date = LocalDate.parse(WoPayData.inquire(group.id), DateTimeFormatter.ISO_DATE).minusDays(1)
+    subscribeAlways<BotInvitedJoinGroupRequestEvent>(priority = Listener.EventPriority.HIGH) {
+        intercept()
+        val date = LocalDate.parse(WoPayData.inquire(this.groupId), DateTimeFormatter.ISO_DATE).minusDays(1)
         if (date.isBefore(LocalDate.now())) {
             launch {
-                logger.info("${group.name}(${group.id}) 邀请入群, 第一次?")
-                delay(TimeUnit.SECONDS.toMillis(15))
-                getGroupOrNull(group.id)?.apply {
+                logger.info("$groupName($groupId) 邀请入群, 第一次?")
+                delay(TimeUnit.SECONDS.toMillis(25))
+                accept()
+                delay(TimeUnit.SECONDS.toMillis(5))
+                getGroupOrNull(groupId)?.apply {
                     getFriendOrNull(SecretConfig.owner)?.sendMessage(
-                        "${group.name}(${group.id}) 邀请入群, 已加入并注册"
+                        "$groupName($groupId) 邀请入群, 已加入并注册"
                     )
                     sendMessage("bot已到期, 如有意愿续费请加群了解:\n117340135\nps: 五分钟后退群")
                     delay(TimeUnit.MINUTES.toMillis(5))
@@ -38,42 +41,11 @@ fun Bot.reaction() {
                 }
             }
         } else {
-            logger.info("加入 ${group.name}(${group.id})")
+            logger.info("加入 $groupName($groupId)")
+            accept()
             getFriendOrNull(owner)?.sendMessage(
-                "${group.name}(${group.id}) 邀请入群, 已加入"
+                "$groupName($groupId) 邀请入群, 已加入"
             )
         }
     }
-
-    subscribeAlways<MemberJoinEvent> {
-        group.sendMessage("欢迎")
-        // TODO 自定义
-    }
-
-//    subscribeAlways<BotLeaveEvent.Kick> {
-//        getFriendOrNull(owner)?.sendMessage(
-//            "已被 ${operator.nameCard}(${operator.id}) 踢出群 ${group.name}(${group.id})"
-//        )
-//    }
-
-//    subscribeAlways<BotLeaveEvent.Active> {
-//        getFriendOrNull(owner)?.sendMessage(
-//            "已离开群 ${group.name}(${group.id})"
-//        )
-//    }
-
-//    subscribeAlways<MemberLeaveEvent.Kick> {
-//        group.sendMessage(
-//            "🔈 ${member.nick}${if (member.nameCard.isEmpty()) "" else "(${member.nameCard})"}被踢出本群 " +
-//                    "\nOperated by ${operator?.nameCardOrNick ?: nick}"
-//        )
-//    }
-
-//    subscribeAlways<MemberLeaveEvent.Quit> {
-//        group.botAsMember.sendMessage("🔈 ${member.nick}(${member.nameCard})退出本群")
-//    } 感觉会很吵
-
-//    subscribeAlways<MemberSpecialTitleChangeEvent> {
-//        group.sendMessage("${member.nameCardOrNick}获得头衔：$new \nAwarded by ${operatorOrBot.nameCardOrNick}")
-//    }
 }
